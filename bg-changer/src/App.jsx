607715196b34dc0c061cc3206fc71d7b76c3d38e
Abhishek <Abhishekraut8697@gmail.com>
2024-06-12 14:19:11 +0530

    created pass generator

--- bg-changer/src/App.jsx
+++ bg-changer/src/App.jsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('salmon');
+  const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'cyan','magenta'];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='w-full h-screen' style={{ backgroundColor: color }}>
+      
+       <div className='flex fixed flex-wrap bottom-12 inset-x-0 justify-center '>
+        <div className='flex flex-wrap bg-white px-4 py-2 rounded-xl gap-3 shadow-lg'>
+          {colors.map((btnCol) => {
+            return <button onClick={()=>{
+              setColor(btnCol)
+            }} className='py-2 rounded-xl gap-3 shadow-lg px-5' style={{backgroundColor: btnCol}}>Red</button>
+          })}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 
